3ae489e01e2e7858dae0cb628e5aa205c1d36fec
kickafetus <masterpoopster@gmail.com>
2026-06-07 00:09:26 -0400

    chore: bump version to 1.0.5 (mod info, script, apworld)

--- mods/dwarfipelago/scripts_modinstalled/dwarfipelago.lua
+++ mods/dwarfipelago/scripts_modinstalled/dwarfipelago.lua
@@ -22,7 +22,7 @@ local eventful   = require("plugins.eventful")
 local repeatUtil = require("repeat-util")
 
 local SCRIPT_NAME = "dwarfipelago"
-local SCRIPT_VERSION = "1.0.4"
+local SCRIPT_VERSION = "1.0.5"
 local POLL_TICKS  = 100  -- poll wealth/trade/goal checks every N ticks
 
 -- Set to true while we are applying a received DeathLink so that the death
