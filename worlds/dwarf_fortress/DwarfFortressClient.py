f728e8a7ec59e70f6546f0370efcaf4d8289c958
jjjj12212 <mjackson12212@hotmail.com>
2026-06-08 09:10:11 -0600

    fix: in-game text when send other players thier items.

--- worlds/dwarf_fortress/DwarfFortressClient.py
+++ worlds/dwarf_fortress/DwarfFortressClient.py
@@ -1003,8 +1003,9 @@ class DwarfFortressContext(CommonContext):
                 if not self.slot_concerns_self(args["receiving"]): # You found someone else's item
                     if self.slot_concerns_self(item.player):
                         to_player = self.player_names[args["receiving"]]
-                        item_name = self.item_names.lookup_in_slot(int(args["data"][2]["text"]), int(args["data"][0]["text"]))
-                        self.dfhack.run_command("lua", f'dfhack.gui.showAnnouncement("You found {to_player} their {item_name}.", COLOR_YELLOW)')
+                        item_name = self.item_names.lookup_in_slot(int(args["data"][2]["text"]), args["receiving"])
+                        #COLOR_BROWN is actually Yellow in this DFHack version
+                        self.dfhack.run_command("lua", f'dfhack.gui.showAnnouncement("You found {to_player} their {item_name}.", COLOR_BROWN)')
                 elif self.slot_concerns_self(args["receiving"]): # This is your item
                     player = self.player_names[int(args["data"][0]["text"])]
                     to_player = self.player_names[args["receiving"]]
