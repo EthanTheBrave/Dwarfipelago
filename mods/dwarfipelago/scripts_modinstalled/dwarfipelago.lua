ee0202f87a59d25659612ec2d31fa23275edd91c
kickafetus <masterpoopster@gmail.com>
2026-06-13 12:35:20 -0400

    chore: bump up versions

--- mods/dwarfipelago/scripts_modinstalled/dwarfipelago.lua
+++ mods/dwarfipelago/scripts_modinstalled/dwarfipelago.lua
@@ -22,7 +22,7 @@ local eventful   = require("plugins.eventful")
 local repeatUtil = require("repeat-util")
 
 local SCRIPT_NAME = "dwarfipelago"
-local SCRIPT_VERSION = "1.1.0"
+local SCRIPT_VERSION = "1.1.1"
 local POLL_TICKS  = 100  -- poll wealth/trade/goal checks every N ticks
 
 local function fmt_energy(j)
